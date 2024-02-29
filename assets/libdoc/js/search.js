cc7213701f27a87c72814ea3ecb06030400372c6
박현우 <hyunwoo@spidor.co.kr>
2024-02-29 15:13:34 +0900

    lunr.js 한글지원 에드온 추가

--- assets/libdoc/js/search.js
+++ assets/libdoc/js/search.js
@@ -44,13 +44,15 @@
 
         // Initalize lunr with the fields it will be searching on. I've given title
         // a boost of 10 to indicate matches on this field are more important.
-        var idx = new lunr.Index;
-        idx.field("id");
-        idx.field("title", { boost: 10 });
-        idx.field("author");
-        idx.field("category");
-        idx.field("tags");
-        idx.field("content");
+        var idx = lunr(function() {
+            this.use(lunr.ko)
+            this.field("id");
+            this.field("title", { boost: 10 });
+            this.field("author");
+            this.field("category");
+            this.field("tags");
+            this.field("content");
+        });
 
         for (var key in window.store) {
             // Add the data to lunr

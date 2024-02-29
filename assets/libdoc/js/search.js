443696de4c3da94ed6fb6e654051afb598a10366
박현우 <hyunwoo@spidor.co.kr>
2024-02-29 15:03:24 +0900

    lunr.js 한국어 지원 이슈 수정

--- assets/libdoc/js/search.js
+++ assets/libdoc/js/search.js
@@ -44,14 +44,13 @@
 
         // Initalize lunr with the fields it will be searching on. I've given title
         // a boost of 10 to indicate matches on this field are more important.
-        var idx = lunr(function() {
-            this.field("id");
-            this.field("title", { boost: 10 });
-            this.field("author");
-            this.field("category");
-            this.field("tags");
-            this.field("content");
-        });
+        var idx = new lunr.Index;
+        idx.field("id");
+        idx.field("title", { boost: 10 });
+        idx.field("author");
+        idx.field("category");
+        idx.field("tags");
+        idx.field("content");
 
         for (var key in window.store) {
             // Add the data to lunr

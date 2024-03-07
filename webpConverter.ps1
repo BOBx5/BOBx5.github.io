390821df679bebd25ca73779671df04ba29b804f
박현우 <hyunwoo@spidor.co.kr>
2024-03-07 19:35:07 +0900

    CREATE Docs_102_CSharp_Regex

--- webpConverter.ps1
+++ webpConverter.ps1
@@ -1,6 +1,6 @@
-$maxNum = 12;
+$maxNum = 1;
 for ($i = 1; $i -lt $maxNum+1; $i++) {
     # Exectue ImageMagick convert to webp => convert "$i.jpg" "$i.webp"
-    Write-Host "Converting '$i.jpg' to '$i.webp'"
+    Write-Host "Converting '$i.png' to '$i.webp'"
     & convert "$i.png" "$i.webp"
 }
